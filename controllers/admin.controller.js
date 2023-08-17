b9e382f7c27014736ca865f8df25199d2c7b34d5
Md. Imran Mir <imranmir6677@gmail.com>
2023-08-18 01:33:43 +0600

    Create product route

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,8 +1,12 @@
-const product = require("../models/products.model");
+const fs = require("fs");
+const Product = require("../models/products.model");
 const config = require("../config/config");
 const adminEmail = config.admin.email;
 const adminPassword = config.admin.password;
 
+//@desc login admin
+//@route POST /api/admin/login
+//@access public
 const adminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -27,6 +31,37 @@ const adminLogin = async (req, res) => {
   }
 };
 
+//@desc Create Product
+//@route POST /api/admin/product
+//@access public
+const createProduct = async (req, res) => {
+  try {
+    const { product_name, product_price, product_description } = req.body;
+
+    const img = req.file ? req.file.filename : null;
+
+    if (!product_name || !product_price || !product_description || !img) {
+      return res.status(400).json("All field are mendatory !");
+    }
+
+    const newProduct = new Product({
+      product_name,
+      product_price,
+      product_description,
+      product_img: {
+        data: fs.readFileSync("uploads/" + img),
+        contentType: "image/png",
+      },
+    });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({ error: "Error creating product." });
+  }
+};
+
 module.exports = {
   adminLogin,
+  createProduct,
 };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
-const { adminLogin } = require("../controllers/admin.controller");
+const {
+  adminLogin,
+  createProduct,
+} = require("../controllers/admin.controller");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.post("/login", adminLogin);
+router.post("/product", upload.single("product_img"), createProduct);
 
 module.exports = router;
